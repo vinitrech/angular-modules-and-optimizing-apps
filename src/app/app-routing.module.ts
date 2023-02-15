3af74b4b832b8d9ffad74b77ece8d85b782f5458
Vinícius Tonello Rech <vinitrech@pop-os.localdomain>
2023-02-14 22:14:05 -0300

    splitting routes into modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,39 +1,23 @@
 import {NgModule} from "@angular/core";
 import {RouterModule, Routes} from "@angular/router";
 import {ShoppingListComponent} from "./shopping-list/shopping-list.component";
-import {RecipeComponent} from "./recipes/recipe.component";
-import {RecipeStartComponent} from "./recipes/recipe-start/recipe-start.component";
-import {RecipeDetailComponent} from "./recipes/recipe-detail/recipe-detail.component";
-import {RecipeEditComponent} from "./recipes/recipe-edit/recipe-edit.component";
-import {RecipesResolverService} from "./shared/recipesResolver.service";
 import {AuthComponent} from "./auth/auth.component";
-import {AuthGuardService} from "./auth/auth-guard.service";
 
 const appRoutes: Routes = [{
-  path: '', redirectTo: '/recipes', pathMatch: "full"
+    path: '', redirectTo: '/recipes', pathMatch: "full"
 },
-  {
-    path: 'shopping-list', component: ShoppingListComponent
-  },
-  {
-    path: 'recipes', component: RecipeComponent,
-    canActivate: [AuthGuardService]
-    , children: [{
-      path: '', component: RecipeStartComponent
+    {
+        path: 'shopping-list', component: ShoppingListComponent
     },
-      {path: 'new', component: RecipeEditComponent},
-      {path: ':id', component: RecipeDetailComponent, resolve: [RecipesResolverService]},
-      {path: ':id/edit', component: RecipeEditComponent, resolve: [RecipesResolverService]}]
-  },
-  {
-    path: 'auth', component: AuthComponent
-  }]
+    {
+        path: 'auth', component: AuthComponent
+    }]
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(appRoutes)
-  ],
-  exports: [RouterModule],
+    imports: [
+        RouterModule.forRoot(appRoutes) // forRoot should be used only once, inside other modules it is correct to use forChild
+    ],
+    exports: [RouterModule],
 })
 export class AppRoutingModule {
 
--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -5,9 +5,9 @@ import {RecipeDetailComponent} from "./recipe-detail/recipe-detail.component";
 import {RecipeStartComponent} from "./recipe-start/recipe-start.component";
 import {RecipeEditComponent} from "./recipe-edit/recipe-edit.component";
 import {RecipeItemComponent} from "./recipe-list/recipe-item/recipe-item.component";
-import {AppRoutingModule} from "../app-routing.module";
 import {CommonModule} from "@angular/common";
 import {ReactiveFormsModule} from "@angular/forms";
+import {RecipesRoutingModule} from "./recipes-routing.module";
 
 @NgModule({
     declarations: [
@@ -18,17 +18,9 @@ import {ReactiveFormsModule} from "@angular/forms";
         RecipeEditComponent,
         RecipeItemComponent,
     ], imports: [
-        AppRoutingModule,
         CommonModule, // Common Module is the BrowserModule, but it should only be used in AppModule. So with CommonModule, we can get access to the ngIf and ngFor, for example.
-        ReactiveFormsModule
-    ],
-    exports: [
-        RecipeComponent,
-        RecipeListComponent,
-        RecipeDetailComponent,
-        RecipeStartComponent,
-        RecipeEditComponent,
-        RecipeItemComponent,
+        ReactiveFormsModule,
+        RecipesRoutingModule
     ]
 })
 export class RecipesModule {
